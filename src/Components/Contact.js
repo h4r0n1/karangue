274102fd84d9a09a2b5ee123324148395b25d443
Cheikh <cheikhahmadouwade@gmail.com>
2021-12-16 15:29:55 +0000

    la page est prete

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,48 +1,54 @@
 import React from "react";
-import { Form, Button } from "react-bootstrap";
-import Footer from "./Footer/Footer";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import FooterPage from "./Footer/Footer";
+
 import NavbarComponent from "./Navbar/Navbar";
 
 const Contact = () => {
   return (
     <div>
       <NavbarComponent />
+      
+      <img src="mage1.jpg" className="Body"/>
+      <h2 className="ecrit">Contact us</h2>
       <div className="contact">
         <h2>Contact</h2>
         <Form>
-          <Form.Group className="mb-3">
-            <Form.Label>
-              <h6>Prenom :</h6>
-            </Form.Label>
-            <Form.Control type="email" placeholder="modou moustapha" />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>
-              <h6>Nom :</h6>
-            </Form.Label>
-            <Form.Control type="email" placeholder="Diakhaby" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>
-              <h6>Entrer votre e-mail :</h6>
-            </Form.Label>
-            <Form.Control type="email" placeholder="karengue@gmail.com" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>
-              <h6>Veillez mettre votre message :</h6>
-            </Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
-        </Form>
-        <div className="button1">
+  <Row className="mb-3">
+    <Form.Group as={Col} >
+      <Form.Label>Email</Form.Label>
+      <Form.Control type=""  />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="" placeholder="" />
+    </Form.Group>
+  </Row>
+  <Row className="mb-4">
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label>Message</Form.Label>
+      <Form.Control type="text" placeholder="mettez votre message" />
+    </Form.Group>
+  </Row>
+
+  
+ 
+
+  <Button variant="primary" type="submit">
+    Envoyer
+  </Button>
+</Form>
+      
+        {/* <div className="button1">
           <Button variant="secondary" href="/">
             Annuler
           </Button>{" "}
+          <h2></h2>
           <Button variant="success">Envoyer</Button>{" "}
-        </div>
+        </div> */}
       </div>
-      <Footer />
+     <FooterPage/>
     </div>
   );
 };
