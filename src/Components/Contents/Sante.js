81a3be21771e134713bf0d36a8e2f1686adc931c
adadiallo <adadialo2203@gmail.com>
2021-12-21 09:01:25 +0100

    modification page sante

--- src/Components/Contents/Sante.js
+++ src/Components/Contents/Sante.js
@@ -1,69 +1,204 @@
-import React from "react";
-import "./Sante.css";
-import { BiSearch } from "react-icons/bi";
-import { BsMic } from "react-icons/bs";
-import NavbarComponent from "../Navbar/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Sante.css';
+import doctor from '../../images/doctor.jpeg'
 
-// Import of animate css
-import "animate.css";
-
-// Import des images
-import hopital from "../../images/hopital.jpg";
-
-const Sante = () => {
-  return (
-    <div>
-      <NavbarComponent />
-      <div className="container-fluid section">
-        <div className="title">
-          <p className="animate__animated animate__fadeInLeftBig">
-            Sante - l'Hopital ou la Pharmacie la plus proche
-          </p>
-        </div>
-
-        <div className="search">
-          <div className="cta_search">
-            <BiSearch />
-            <input type="search" placeholder="Recherche" />
-            <BsMic />
-          </div>
-        </div>
-
-        {/** == == == Section Hopitaux == == == */}
-        <div id="hopitaux" className="section">
-          <div id="myHospital">
-            <div className="card">
-              <img src={hopital} alt="Hopiatal" />
-              <div className="cardContent">
-                <p>Dakar-Grand-Yoff</p>
-              </div>
-            </div>
-
-            <div className="card">
-              <img src={hopital} alt="Hopiatal" />
-              <div className="cardContent">
-                <p>Dakar-Grand-Yoff</p>
-              </div>
-            </div>
-
-            <div className="card">
-              <img src={hopital} alt="Hopiatal" />
-              <div className="cardContent">
-                <p>Dakar-Grand-Yoff</p>
-              </div>
-            </div>
-
-            <div className="card">
-              <img src={hopital} alt="Hopiatal" />
-              <div className="cardContent">
-                <p>Dakar-Grand-Yoff</p>
-              </div>
-            </div>
-          </div>
-        </div>
+function Sante () {
+  return(
+    <div className=' container-sante'>
+      <div className='container-fluid sante justify-content-center pr-2'>
+        <h1 className='title'>Sante</h1>
+        <p className='para-sante'>Accueil > Sante</p>
       </div>
-    </div>
-  );
-};
+      <div className='container' >
+      <div className=' row section-cards text-center '>
+      <div className='col-lg-3'>
+      <div className="card ">
+      <div className=''>
+      <img src= {doctor} className='image'/>
+      </div>
+   
+  <div class="card-body">
+    <h5 className="card-title title-card ">Mèdecin Expert</h5>
+    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
 
+</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+
+            
+  <div className='col-lg-3'>
+      <div className="card ">
+      <div className=''>
+      <img src= {doctor} className='image'/>
+      </div>
+   
+  <div class="card-body">
+    <h5 className="card-title title-card ">Diagnostic</h5>
+    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+
+</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+
+
+
+  <div className='col-lg-3'>
+
+<div className="card ">
+      <div className=''>
+      <img src= {doctor} className='image'/>
+      </div>
+   
+  <div class="card-body">
+    <h5 className="card-title title-card ">Pathologie</h5>
+    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+
+</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+
+
+<div className='col-lg-3'>
+
+
+<div className="card ">
+      <div className=''>
+      <img src= {doctor} className='image'/>
+      </div>
+   
+  <div class="card-body">
+    <h5 className="card-title title-card ">Soins Dentaires</h5>
+    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+
+</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+
+      <div className=' row section-cards text-center '>
+      <div className='col-lg-3'>
+      <div className="card ">
+      <div className=''>
+      <img src= {doctor} className='image'/>
+      </div>
+   
+  <div class="card-body">
+    <h5 className="card-title title-card ">Cardiologie</h5>
+    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+
+</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+ </div> 
+
+ <div className='col-lg-3'>    
+      <div className="card">
+      <div className=''>
+      <img src= {doctor} className='image'/>
+      </div>
+   
+  <div class="card-body">
+    <h5 className="card-title title-card ">Mèdecine</h5>
+    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+
+</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+
+
+<div className='col-lg-3'> 
+<div className="card ">
+      <div className=''>
+      <img src= {doctor} className='image'/>
+      </div>
+   
+  <div class="card-body">
+    <h5 className="card-title title-card ">Neurologie</h5>
+    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+
+</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+
+
+<div className='col-lg-3'> 
+<div className="card ">
+      <div className=''>
+      <img src= {doctor} className='image'/>
+      </div>
+   
+  <div class="card-body">
+    <h5 className="card-title title-card ">Ambulance</h5>
+    <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+
+</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+    </div>
+
+  )
+}
 export default Sante;
